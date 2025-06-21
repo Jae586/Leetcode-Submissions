1e4c2419da98fb47026ac35c3167c2f248ad206d
Jae586 <87919104+Jae586@users.noreply.github.com>
2025-06-21 09:29:48 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int left = 0; 
+        int right = s.length()-1; 
+        
+        while(left < right){
+            char le = s.charAt(left);
+            char ri = s.charAt(right);
+            while (!Character.isLetterOrDigit(le)){
+                left++; 
+                 le = s.charAt(left);
+            }
+            while (!Character.isLetterOrDigit(ri)){
+                right--;
+                 ri = s.charAt(right);
+            }
+
+            if (Character.toLowerCase(le) != Character.toLowerCase(ri)){
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
